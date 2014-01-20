a5c63ce2bd162344bd87adc49badff55fd948572
pat_mic <pat_mic@pat-mic>
2014-01-20 14:03:40 -0500

    Persona , modificación.

---
+++ fuel/app/config/crypt.php
@@ -0,0 +1,6 @@
+<?php
+return array(
+	'crypto_key' => 'S1c68IoDAaiU2KoleYdAASlc',
+	'crypto_iv' => 'XM4z8AMl89KMO-oJOMdrQzvk',
+	'crypto_hmac' => 'toYbV88C8ups4pgswIkqY8e0',
+);
---
+++ fuel/app/config/db.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Use this file to override global defaults.
+ *
+ * See the individual environment DB configs for specific config information.
+ */
+
+
+return array(
+	'default' => array(
+		'connection'  => array(
+			'dsn'        => 'mysql:host=127.0.0.1;dbname=prueba',
+			'username'   => 'root',
+			'password'   => '',
+		),
+	),
+);
